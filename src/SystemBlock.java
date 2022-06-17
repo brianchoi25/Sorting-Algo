cd99ee13eb4a45cb114da4315433e207cdce5e5b
Brian <choibrian25@gmail.com>
2022-06-16 21:34:19 -0700

    Added basic ideas of what sort of files I wanted to implement. I am mostly getting familiar and using knowledge from Digital Signal Processing classes to implement a couple things.

--- src/SystemBlock.java
+++ src/SystemBlock.java
@@ -1,25 +1,53 @@
 public class SystemBlock implements I_filter{
 
     double[] systemResponse;
+    double[] result;
+    SystemBlock prev; // Allows connections to other system blocks for input
+    SystemBlock next; // Allows connections to other system blocks for output
+
 
     //----------Constructor pass System response-------------
     public SystemBlock(double input[]){
-        this.systemResponse = input;
+        if( this.prev != null) {
+            this.systemResponse = this.prev.getSystemResponse();
+        }else{
+            this.systemResponse = input;
+        }
+
     }
+
+    public SystemBlock(){
+        if( this.prev != null) {
+            this.systemResponse = this.prev.getSystemResponse();
+        }
+    }
+
+    public double[] getSystemResponse(){return systemResponse;}
+
+    public void addToEnd(SystemBlock A){
+        this.next = A;
+        A.prev = this;
+        A.systemResponse = this.result;
+    }
+
+    public void setNext(SystemBlock A ){this.next = A;} //Sets the next block
+
+    public void setPrev(SystemBlock A ){this.prev = A;} //Sets the previous block
 
     public double[] conv(double inputSignal[]){
 
         int n = systemResponse.length;
         int m = inputSignal.length;
 
-        double[] result = new double[(n+m-1)];
+        this.result = new double[(n+m-1)];
 
         for(int i = 0; i< n; i++){
             for(int j = 0; j < m; j++){
-                result[i+j] += (systemResponse[i]*inputSignal[j]);
+                this.result[i+j] += (systemResponse[i]*inputSignal[j]);
             }
         }
-        return result;
+        return this.result;
     }
+
 
 }
--- src/source.java
+++ src/source.java
@@ -2,9 +2,18 @@ public class source {
     public static void main(String[] args) {
 
         double[] testArray = new double[]{3,2,1};
+        double[] testArray2 = new double[]{1,1,1};
         double[] input = new double[]{1,2,3,4,5};
 
         SystemBlock A = new SystemBlock(testArray);
-        System.out.println(A.conv(input));
+
+        SystemBlock B = new SystemBlock(testArray2);
+        A.conv(input);
+        A.addToEnd(B);
+        B.conv(testArray2);
+
+        System.out.println(A.result);
+        System.out.println(B.result);
+        System.out.println("Done");
     }
 }
